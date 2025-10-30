6fcf50e38495cfcddfdc81fdffc724578a41cc26
Yash Kumar Sharma <ykumar2502@gmail.com>
2025-10-30 18:10:50 +0530

    Fix FVF import: replaced add_fvf_labels with sky130_add_fvf_labels

--- src/glayout/blocks/elementary/__init__.py
+++ src/glayout/blocks/elementary/__init__.py
@@ -1,4 +1,4 @@
 from .transmission_gate import transmission_gate, tg_netlist, add_tg_labels
 from .diff_pair import diff_pair, diff_pair_generic, diff_pair_netlist
 from .current_mirror import current_mirror, current_mirror_netlist
-from .FVF import flipped_voltage_follower, fvf_netlist, add_fvf_labels
+from .FVF import flipped_voltage_follower, fvf_netlist, sky130_add_fvf_labels
